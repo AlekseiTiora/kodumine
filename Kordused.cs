882f322c22bb5a3002fcd6599c37510db1d533a6
AlekseiTiora <77392666+AlekseiTiora@users.noreply.github.com>
2021-10-12 09:20:42 +0300

    Update Kordused.cs

--- Kordused.cs
+++ Kordused.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -37,7 +37,7 @@ namespace kodumine
                 nr++;
             } while (nr!=nimed.Length) ;*/
 
-             Random rnd = new Random();
+             /*Random rnd = new Random();
              int N = rnd.Next(2, 6);
              int M = rnd.Next(7, 14);
              int [] mass  = new int[M - N];
@@ -55,10 +55,58 @@ namespace kodumine
             foreach (var m in mass)
             {
                 Console.WriteLine(m);
-            }
+            }*/
+           /* for (int i = 0; i < 5; i++)
+			{
+                 Console.WriteLine("kirjutage 5 numbrit");
+                int smt = Convert.ToInt32(Console.ReadLine());
+                arr[i] = smt;
+			}
+            int sum = 0;
+            int mult = 1;
+            foreach (var item in arr)
+	        {
+                sum = item;
+                mult = item + mult;
+	        }
+            Console.WriteLine($"hums AVG= {arr.Average()}");*/
 
-            Console.WriteLine("kirjutage 5 numbrit");
-            Console.ReadKey();
+
+
+            /*int sum = 0;
+            int mult = 1;
+            for (int i = 0; i < 5; i++)
+			{
+                Console.Write("Kirjuta number");
+                int smt = Convert.ToInt32(Console.ReadLine());
+                sum +=smt;
+                mult=smt * mult;
+			}
+            double avg= sum /5;
+            Console.WriteLine($"Nums AVG = {avg}\nNums SUM = {sum}\nNums multipiled = {mult}");*/
+
+            /*int nr=0;
+            
+            string otvet;
+           do
+            {
+               Console.WriteLine("Osta elevant ära!");
+               Console.Write("ostad?");
+               Console.WriteLine("");
+               otvet = Console.ReadLine();
+
+            }while(otvet!="elevant");
+            Console.WriteLine("sa ostsid elevandi ");*/
+
+            Console.WriteLine("Arvuti mõtles number");
+            do{
+              }
+             
+
+
+
+
+
 
 
 
